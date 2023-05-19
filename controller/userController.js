8a1b18556ffddeb1f59b3c5f03103e4954c79633
shbhm96 <shbhm96@gmail.com>
2023-05-19 13:09:00 +0530

    update user profile

--- controller/userController.js
+++ controller/userController.js
@@ -32,7 +32,7 @@ const getUserProfile = asyncHandler(async(req,res) => {
     const user = await User.findById(req.user._id)
 
     if(user ){
-        return res.json({
+        res.json({
             _id : user._id,
             name : user.name,
             email:user.email,
@@ -79,4 +79,32 @@ const createUser = asyncHandler(async(req,res) => {
     }
     console.log("Create User8   `729")
 })
-export {authUser,getUserProfile,createUser}
+
+const updateUserProfile = asyncHandler(async(req,res) => {
+    const user = await User.findById(req.user._id)
+
+    if(user ){
+       user.name = req.body.name || user.name
+       user.email = req.body.email || user.email      
+       if(req.body.password){
+        user.password = req.body.password
+       }
+
+       const updateUser = await user.save()
+
+       res.json({
+        _id : updateUser._id,
+        name : updateUser.name,
+        email:updateUser.email,
+        isAdmin : updateUser.isAdmin,
+        token : generateToken(updateUser._id)
+    })
+    }else{
+        res.status(404)
+        throw new Error ('User Not Found')
+    }
+
+    res.send({email,password})
+})
+
+export {authUser,getUserProfile,createUser,updateUserProfile}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,16 @@
 import express from "express";
-import { authUser, createUser, getUserProfile } from "../controller/userController.js";
+import 
+{ 
+    authUser, 
+    createUser, 
+    getUserProfile, 
+    updateUserProfile 
+} from "../controller/userController.js";
 import protectValidUser from "../middleware/authMiddleWare.js";
-
-
-
 const router = express.Router()
 
 router.post("/login",authUser)
-router.get("/profile",protectValidUser,getUserProfile)
+router.get("/profile",protectValidUser,getUserProfile).put(protectValidUser,updateUserProfile)
 router.post("/",createUser)
 
 export default router
